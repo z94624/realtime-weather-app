72d9423507c74293d02b4e703b156b3dc0a3c7e9
smoBEE <z94624@gmail.com>
2021-05-12 11:06:02 +0800

    利用 PWA 將網頁變成手機 Web App

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ import './index.css';
 import 'normalize.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// 把即時天氣 APP 包成 PWA(Progressive Web App)
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,3 +22,9 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+/*
+ * Service Worker 可以把網頁應用程式暫存(cache)下來，提升瀏覽速度
+ * 讓網頁就像 App 一樣不用透過 Google Play 就安裝在手機上
+ */
+serviceWorker.register();
